cecdbd0c0cb47a49eeeaa9cc1f7a7c993413d650
fzq <fzq@example.com>
2018-06-25 15:09:34 +0800

    fixed-version

--- src/test/java/top/fzqblog/ant/worker/TaskWorkerTest.java
+++ src/test/java/top/fzqblog/ant/worker/TaskWorkerTest.java
@@ -1,7 +1,9 @@
 package top.fzqblog.ant.worker;
 
 import org.junit.Test;
+import top.fzqblog.ant.handler.ErrorHandler;
 import top.fzqblog.ant.monitor.AntMonitor;
+import top.fzqblog.ant.pipeline.SubPipeline;
 import top.fzqblog.ant.queue.AntQueue;
 import top.fzqblog.ant.queue.TaskQueue;
 import top.fzqblog.ant.task.Task;
@@ -16,7 +18,12 @@ public class TaskWorkerTest {
         try {
             AntQueue antQueue = TaskQueue.of();
             antQueue.push(new Task("https://github.com/"));
-            Ant ant = Ant.create().startQueue(antQueue).thread(1);
+            Ant ant = Ant
+                    .create()
+                    .startQueue(antQueue)
+                    .pipeline(new SubPipeline())
+                    .withHandler(new ErrorHandler())
+                    .thread(1);
             AntMonitor.getInstance().regist(ant);
             ant.run();
         } catch (Exception e) {
